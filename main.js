c16015c82d6c898bbf6016ab2d87d500fc3a10c3
Bogdan Romanov <kaperlinkor@yandex.ru>
2025-01-12 21:58:10 +0300

    Старт проекта

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const process = require('process')
 
 const username = process.argv.slice(2, 3)
 
-fetch(`https://api.github.com/users/${username}`)
+// fetch(`https://api.github.com/users/${username}`)
