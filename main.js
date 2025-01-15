417310e67d162f38892167f5299f713b54a2e77e
Bogdan Romanov <kaperlinkor@yandex.ru>
2025-01-15 22:23:58 +0300

    старт проекта

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ const process = require('process')
 
 const username = process.argv.slice(2, 3)
 
-// fetch(`https://api.github.com/users/${username}`)
+fetch(`https://api.github.com/users/${username[0]}`)
+    .then(res => res.json())
+    .then(res => console.log(res.public_repos))
+    .catch(err => console.log(`Ошибка запроса :c`, err.message))
+
