53f93c57649dff1adc50bf2669810032a61d2d14
Bogdan Romanov <kaperlinkor@yandex.ru>
2025-01-19 22:01:10 +0300

    Add array of all events

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const process = require('process')
 
 const username = process.argv.slice(2, 3)
 
-fetch(`https://api.github.com/users/${username[0]}`)
+fetch(`https://api.github.com/users/${username[0]}/events`)
     .then(res => {
         if (!res.ok) {
             throw new Error(`Ошибка запроса, блин :c. ${res.status}`)
         }
         return res.json()
         })
-    .then(data => console.log(data.public_repos))
+    .then(data => console.log(data))
 
+const events = ['WatchEvent', 'CommitCommentEvent', 'PushEvent', 'CreateEvent', ]
