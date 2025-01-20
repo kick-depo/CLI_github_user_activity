8e8c55d4a224675a135d9ddb3ce41b1c463efe45
Bogdan Romanov <kaperlinkor@yandex.ru>
2025-01-21 00:04:05 +0300

    Создание асинхронной функции

--- main.js
+++ main.js
@@ -3,29 +3,32 @@ const process = require('process')
 const username = process.argv.slice(2, 3)
 const events = ['WatchEvent', 'CommitCommentEvent', 'CreateEvent', 'DeleteEvent', 'ForkEvent', 'GollumEvent', 'IssueCommentEvent', 'IssuesEvent', 'MemberEvent', 'PublicEvent', 'PullRequestEvent', 'PullRequestReviewEvent', 'PullRequestReviewCommentEvent', 'PullRequestReviewThreadEvent', 'PushEvent', 'ReleaseEvent', 'SponsorshipEvent']
 
-const userActiviti = fetch(`https://api.github.com/users/${username[0]}/events`)
+async function getUserActiviti() {
+    const userActiviti = await fetch(`https://api.github.com/users/${username[0]}/events`)
     .then(res => {
         if (!res.ok) {
             throw new Error(`Ошибка запроса, блин :c. ${res.status}`)
         }
         return res.json()
         })
-    // .then(data => console.log(data))
 
-let eventsActivity = []
-let count = 0
+    let eventsActivity = []
+    let count = 0
 
-for (let i = 0; i < userActiviti.length; i++) {
-    for (let j = 0; j < userActiviti.length; j++) {
-        if (userActiviti[i].type === events[i]) {
-            count++
+    for (let i = 0; i < userActiviti.length; i++) {
+        // использовать events.length
+        for (let j = 0; j < userActiviti.length; j++) {
+            if (userActiviti[i].type === events[i]) {
+                count++
+            }
+            eventsActivity.push({[events[i]]:count})
+            count = 0
         }
-        eventsActivity.push({[events[i]]:count})
-        count = 0
     }
+    return eventsActivity
 }
 
-// console.log(eventsActivity)
-console.log(userActiviti.length)
+// const output = getUserActiviti()
+getUserActiviti()
 
 
